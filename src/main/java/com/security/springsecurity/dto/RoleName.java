b62784f0f3aeb4f34034d261ef54ad955424a6cb
tezeethiopia3 <tezeethiopia3@gmail.com>
2024-11-28 22:15:53 +0300

    commit4 by tezera4

---
+++ src/main/java/com/security/springsecurity/dto/RoleName.java
@@ -0,0 +1,14 @@
+package com.security.springsecurity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RoleName {
+    private String name;
+}
